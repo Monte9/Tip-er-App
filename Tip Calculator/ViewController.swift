135ac4b65a1cfc58594a91c0960ab772bf1fdc6b
Monte Thakkar <thakkarmanthan9@yahoo.com>
2015-12-29 17:27:28 -0800

    Basic functioning of the app complete.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var inputSliderTip: UISlider!
 
+    @IBOutlet weak var tipLabel: UILabel!
+
+    @IBOutlet weak var mealCostTextField: UITextField!
+    
+    @IBOutlet weak var finalCostLabel: UILabel!
+    
+    var mealCost : Float?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.mealCostTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        let tipPercent = Int(sender.value)
+        self.tipLabel.text = "\(tipPercent)"
+        updateFinalCost()
     }
 
+    @IBAction func newCostEntered(sender: UITextField) {
+        self.mealCost = Float(sender.text!)
+        updateFinalCost()
+    }
+    
+    func finalCost() -> Float {
+        let tipValue =  (mealCost! * Float(tipLabel.text!)! ) / 100
+        return (tipValue + mealCost!)
+    }
+    
+    func updateFinalCost() {
+        if mealCost != nil {
+            self.finalCostLabel.text = "\(self.finalCost())"
+        } else {
+            self.finalCostLabel.text = "invalid cost"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
